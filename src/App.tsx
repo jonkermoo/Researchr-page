c6b814818a3cb00e386be4faec0ce1f8d53387fb
Johnny Zhu <zhu.johnny0@gmail.com>
2025-07-28 23:42:24 -0400

    additional folders for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/pages/Header";
+import Footer from "./components/pages/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes></Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
